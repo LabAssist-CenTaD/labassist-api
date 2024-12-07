b06004266920a0a606adbd9e999ede3802c42ced
ChronoVortex07 <202344M@gmail.com>
2024-12-07 21:58:30 +0800

    Added route for downloading video

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -1,6 +1,6 @@
 import os
 import jsonpatch
-from flask import Blueprint, jsonify, request, current_app
+from flask import Blueprint, jsonify, request, current_app, Response
 from werkzeug.utils import secure_filename
 
 from app.services.video_analysis import analyze_clip, get_task_status
@@ -69,6 +69,37 @@ def process_video(clip_name):
     task_result = analyze_clip(device_id, clip_name, cleanup=current_app.config['CLEANUP_UPLOADS'])
     vjm.add_task(device_id, clip_name, task_result.id)
     return jsonify({'task_id': task_result.id}), 202
+
+@video_routes.route('/video/<clip_name>', methods=['GET'])
+def get_video(clip_name):
+    """Route to get a video clip.
+    Args:
+        clip_name (str): The name of the video clip to get.
+        device_id (str): The ID of the device that uploaded the video.
+    Returns:
+        response (file): The video file to download
+    """
+    device_id = request.args.get('device_id')
+    vjm = current_app.extensions['vjm']
+    if not device_id:
+        return jsonify({'message': 'device ID is required'}), 400
+    elif device_id not in vjm.video_json['videos']:
+        return jsonify({'message': 'device ID not found'}), 404
+    elif device_id not in os.listdir(current_app.config['UPLOAD_FOLDER']) or clip_name not in os.listdir(current_app.config['UPLOAD_FOLDER'] / device_id) or clip_name not in [video['file_name'] for video in vjm.get_device_videos(device_id)]:
+        vjm.sync_videos(current_app.config['UPLOAD_FOLDER'])
+        return jsonify(
+            {
+                'message': 'Video not found or session expired. Please upload the video again',
+                'available_videos': [video['file_name'] for video in vjm.get_device_videos(device_id)]
+            }
+        ), 404
+    with open(current_app.config['UPLOAD_FOLDER'] / device_id / clip_name, 'rb') as f:
+        return Response(
+            f.read(), 
+            mimetype='video/mp4',
+            content_type='video/mp4',
+            headers={'Content-Disposition': f'attachment; filename={clip_name}'}
+        )
 
 # This function is currently not in use as polling is done through the socket connection instead
 @video_routes.route('/get_task_status/<clip_name>', methods=['GET'])
